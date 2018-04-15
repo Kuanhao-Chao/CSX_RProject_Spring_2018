f5ea894a424f08f2107673f77e8462b08c4eb12d
Kuan-Hao, Chao <ntueeb05howard@gmail.com>
2018-04-15 11:42:26 +0800

    Add a simple package

---
+++ week_6/self_practice/simple_r_package/R/hello.R
@@ -0,0 +1,16 @@
+#' Hello wolrd function
+#'
+#' Pint Hello, world!
+#' @param x A numeric value to be squared
+#' @return The square of the input
+#' @export
+square <- function(x) {
+  return(x^2)
+}
+
+#' @export
+hello <- function() {
+  print("Hello, world!")
+}
+
+
